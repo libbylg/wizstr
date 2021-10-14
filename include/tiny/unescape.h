b1f596df7049c3261a6dcd920f600675ba755acd
libbylg <libbylg@126.com>
2021-10-14 22:45:51 +0800

    添加License

--- include/tiny/unescape.h
+++ include/tiny/unescape.h
@@ -1,7 +1,12 @@
-//
-// Created by luolijun on 2021/10/1.
-//
-
+/**
+ * Copyright (c) 2021-2021 libbylg@126.com
+ * tiny is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
 #ifndef UNESCAPE_H
 #define UNESCAPE_H
 
@@ -19,4 +24,4 @@ public:
 }
 
 } // namespace tiny
-#endif //UNESCAPE_H
+#endif // UNESCAPE_H
