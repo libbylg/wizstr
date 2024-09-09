6b0630685e24a5772946c87d5aa92cfec2bc768b
liugang <libbylg@126.com>
2024-09-09 13:01:51 +0800

    repair some test cases

--- test/test_view_split_lines.cpp
+++ test/test_view_split_lines.cpp
@@ -33,7 +33,8 @@ TEST_CASE("view::split_lines:keep_ends=false") {
             return 0;
         });
 
-        REQUIRE(eq(expect, result));
+        REQUIRE(result == expect);
+        REQUIRE(view::split_lines(s, false) == expect);
     }
     SECTION("空串") {
         std::string s{""};
@@ -46,6 +47,7 @@ TEST_CASE("view::split_lines:keep_ends=false") {
         });
 
         REQUIRE(expect == result);
+        REQUIRE(view::split_lines(s, false) == expect);
     }
     SECTION("各种换行场景") {
         std::string s{"\n\r\r\n\r"};
@@ -58,6 +60,7 @@ TEST_CASE("view::split_lines:keep_ends=false") {
         });
 
         REQUIRE(result == expect);
+        REQUIRE(view::split_lines(s, false) == expect);
     }
     SECTION("综合测试") {
         std::string s{"a\n\ryyy\r\r\nb\n\nc\n\r"};
@@ -70,57 +73,101 @@ TEST_CASE("view::split_lines:keep_ends=false") {
         });
 
         REQUIRE(result == expect);
+        REQUIRE(view::split_lines(s, false) == expect);
     }
 }
 
 TEST_CASE("view::split_lines:keep_ends=true") {
     SECTION("简单场景") {
         std::string s{"\na\r\nb\ncccc\n"};
-        std::vector<std::string> expect = {"\n", "a\r\n", "b\n", "cccc\n"};
+        std::vector<std::string_view> expect = {"\n", "a\r\n", "b\n", "cccc\n"};
 
-        std::vector<std::string> result;
+        std::vector<std::string_view> result;
         view::split_lines(s, true, [&result](std::string_view item) -> int {
             result.emplace_back(item);
             return 0;
         });
 
         REQUIRE(result == expect);
+        REQUIRE(view::split_lines(s, true) == expect);
     }
     SECTION("空串") {
         std::string s{""};
-        std::vector<std::string> expect = {};
+        std::vector<std::string_view> expect = {};
 
-        std::vector<std::string> result;
+        std::vector<std::string_view> result;
         view::split_lines(s, true, [&result](std::string_view item) -> int {
             result.emplace_back(item);
             return 0;
         });
 
         REQUIRE(result == expect);
+        REQUIRE(view::split_lines(s, true) == expect);
     }
     SECTION("纯换行") {
         std::string s{"\n"};
-        std::vector<std::string> expect = {"\n", ""};
+        std::vector<std::string_view> expect = {"\n"};
 
-        std::vector<std::string> result;
+        std::vector<std::string_view> result;
         view::split_lines(s, true, [&result](std::string_view item) -> int {
             result.emplace_back(item);
             return 0;
         });
 
         REQUIRE(result == expect);
+        REQUIRE(view::split_lines(s, true) == expect);
     }
 
     SECTION("综合测试") {
         std::string s{"a\n\ryyy\r\r\nb\n\nc\n\r"};
-        std::vector<std::string> expect = {"a\n", "\r", "yyy\r", "\r\n", "b\n", "\n", "c\n", "\r"};
+        std::vector<std::string_view> expect = {"a\n", "\r", "yyy\r", "\r\n", "b\n", "\n", "c\n", "\r"};
 
-        std::vector<std::string> result;
+        std::vector<std::string_view> result;
         view::split_lines(s, true, [&result](std::string_view item) -> int {
             result.emplace_back(item);
             return 0;
         });
 
         REQUIRE(result == expect);
+        REQUIRE(view::split_lines(s, true) == expect);
+    }
+    SECTION("最后一行没有换行符号") {
+        std::string s{"a\nyyy\nb\n\ncdef"};
+        std::vector<std::string_view> expect = {"a\n", "yyy\n", "b\n", "\n", "cdef"};
+
+        std::vector<std::string_view> result;
+        view::split_lines(s, true, [&result](std::string_view item) -> int {
+            result.emplace_back(item);
+            return 0;
+        });
+
+        REQUIRE(result == expect);
+        REQUIRE(view::split_lines(s, true) == expect);
+    }
+    SECTION("单行无换行符") {
+        std::string s{"ayyynbncdef"};
+        std::vector<std::string_view> expect = {"ayyynbncdef"};
+
+        std::vector<std::string_view> result;
+        view::split_lines(s, true, [&result](std::string_view item) -> int {
+            result.emplace_back(item);
+            return 0;
+        });
+
+        REQUIRE(result == expect);
+        REQUIRE(view::split_lines(s, true) == expect);
+    }
+    SECTION("提前终止") {
+        std::string s{"a\nyyy\nb\n\ncdef"};
+        std::vector<std::string_view> expect = {"a\n", "yyy\n", "b\n"};
+
+        std::vector<std::string_view> result;
+        view::split_lines(s, true, [&result](std::string_view item) -> int {
+            result.emplace_back(item);
+            return (result.size() < 3 ? 0 : -1);
+        });
+
+        REQUIRE(result == expect);
+        REQUIRE(view::split_lines(s, true) == std::vector<std::string_view>{"a\n", "yyy\n", "b\n", "\n", "cdef"});
     }
 }
