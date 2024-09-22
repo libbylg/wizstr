c1e0f09947c7e1f64735da353b2b8cf5565f11aa
liugang <libbylg@126.com>
2024-09-22 22:45:02 +0800

    repair some test cases

---
+++ test/test_view_is_blank.cpp
@@ -0,0 +1,13 @@
+#include "catch2/catch_all.hpp"
+
+#include "str.h"
+#include "view.h"
+
+TEST_CASE("view::is_blank") {
+    REQUIRE(view::is_blank("\r\n\v\t ") == false);
+    REQUIRE(view::is_blank("") == false);
+    REQUIRE(view::is_blank(" \t\t ") == true);
+    REQUIRE(view::is_blank("XYZ#") == false);
+    REQUIRE(view::is_blank("\t  XYZ#") == false);
+    REQUIRE(view::is_blank("XYZ#\t  ") == false);
+}
--- test/test_view_is_space.cpp
+++ test/test_view_is_space.cpp
@@ -7,4 +7,5 @@ TEST_CASE("view::is_space") {
     REQUIRE(view::is_space("\r\n\v\t ") == true);
     REQUIRE(view::is_space("") == false);
     REQUIRE(view::is_space("123DEF") == false);
+    REQUIRE(view::is_space("\r\n\v\t #") == false);
 }
