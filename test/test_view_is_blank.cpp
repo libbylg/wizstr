fac562c4fab59a7babd40fe20ca29276f13e878d
liugang <libbylg@126.com>
2024-09-22 22:54:10 +0800

    repair some test cases

--- test/test_view_is_blank.cpp
+++ test/test_view_is_blank.cpp
@@ -4,7 +4,7 @@
 #include "view.h"
 
 TEST_CASE("view::is_blank") {
-    REQUIRE(view::is_blank(" \t\\t ") == true);
+    REQUIRE(view::is_blank(" \t\t ") == true);
     REQUIRE(view::is_blank("\r\n\v\t ") == false);
     REQUIRE(view::is_blank("") == false);
     REQUIRE(view::is_blank("XYZ#") == false);
