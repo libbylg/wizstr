53b0a8bb69aacb623010a2b42d897ffad063d1ea
liugang <libbylg@126.com>
2024-11-21 23:09:51 +0800

    repair some test cases

--- test/test_view_wildcmp.cpp
+++ test/test_view_wildcmp.cpp
@@ -6,6 +6,11 @@
 TEST_CASE("view::wildcmp") {
     SECTION("检查是否匹配某个正则表达式") {
         REQUIRE(view::wildcmp("3*", "3bc12def33"));
+        REQUIRE(view::wildcmp("3abc", "3abc"));
+        REQUIRE(view::wildcmp("?", "3"));
+        REQUIRE(view::wildcmp("????", "3abc"));
+        REQUIRE(view::wildcmp("", "3abc") == false);
+        REQUIRE(view::wildcmp("", ""));
         REQUIRE(view::wildcmp("*3", "3bc12def33"));
         REQUIRE(view::wildcmp("*3?", "3bc12def33"));
         REQUIRE(view::wildcmp("*?3", "3bc12def33"));
