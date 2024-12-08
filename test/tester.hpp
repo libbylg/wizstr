bac627d7cd58a13bff695424038ace0a3607230b
liugang <libbylg@126.com>
2024-12-08 08:00:52 +0800

    switch from catch2 to gtest

--- test/tester.hpp
+++ test/tester.hpp
@@ -1,6 +1,10 @@
 #ifndef TESTER_H
 #define TESTER_H
 
+#define UT_WITH_GTEST 0
+#define UT_WITH_CATCH2 1
+
+#if TEST_FRAMEWORK == UT_WITH_GTEST
 #include "gtest/gtest.h"
 
 #define SECTION(text)
@@ -19,5 +23,14 @@ auto operator==(const std::vector<T>& a, const std::vector<T>& b) -> bool {
 
     return true;
 }
+#elif TEST_FRAMEWORK == UT_WITH_CATCH2
+#include "catch2/catch_all.hpp"
+#define TEST(SuiteName_, CaseName_) TEST_CASE(#SuiteName_ "__" #CaseName_)
+#define ASSERT_EQ(a, b) REQUIRE((a) == (b))
+#define ASSERT_TRUE(a) REQUIRE(a)
+#define ASSERT_FALSE(a) REQUIRE(!(a))
+#else
+#error "Unsupported test framework7"
+#endif
 
 #endif//TESTER_H
