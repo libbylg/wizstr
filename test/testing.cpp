59fa575fc3f359662eb67361b27e1ca0130e56f3
libbylg@126.com <libbylg@126.com>
2025-05-07 23:52:33 +0800

    sync testing

--- test/testing.cpp
+++ test/testing.cpp
@@ -1,5 +1,5 @@
 #include "testing.hpp"
 
 int main(int argc, char* argv[]) {
-    return testing::exec_all(argc, argv);
+    return TESTING_TESTCACSE_CLASS::exec_all(argc, argv);
 }
--- test/testing.hpp
+++ test/testing.hpp
@@ -39,7 +39,7 @@ struct testcase_head {
 //! 测试用例
 struct testcase : public testcase_head {
     // 测试用例函数类型定义
-    using testproc = void (*)(testcase* tcase, std::jmp_buf tenv);
+    using testproc = void (*)();
 
     // 测试用例函数地址
     testproc proc;
@@ -52,6 +52,11 @@ struct testcase : public testcase_head {
     const char* file;
     size_t lineno;
 
+    //! 所有测试用例表
+    static inline testcase_head all_testcases{};
+    static inline testcase* curr_testcase{nullptr};
+    static jmp_buf curr_assertion_env;
+
     //! 测试用例构造函数
     explicit testcase(testproc proc, const char* nsuite, const char* ncase, const char* file, size_t lineno)
         : proc{proc}
@@ -62,15 +67,35 @@ struct testcase : public testcase_head {
         insert(this, all_testcases.prev, &all_testcases);
     }
 
+    //! 执行一个测试用例
+    static inline bool exec(testcase* tcase, jmp_buf& tenv, bool& abort) {
+        // 每个用例执行前打印下当前正在执行那个任务
+        std::printf("%s[%s - %s] %s%s\n", tag_blue_enter, tcase->nsuite, tcase->ncase, "....", tag_blue_leave);
+
+        // 设置 longjmp 的会跳点，以便于断言宏在必要时中断用例执行
+        if (setjmp(tenv)) {
+            std::printf("%s[%s - %s] %s%s\n", tag_fail_enter, tcase->nsuite, tcase->ncase, "fail", tag_fail_leave);
+            abort = true;
+            return false;
+        }
+
+        // 执行用例
+        tcase->proc();
+
+        // 如果执行成功，proc 函数会自动返回
+        std::printf("%s[%s - %s] %s%s\n", tag_pass_enter, tcase->nsuite, tcase->ncase, "pass", tag_pass_leave);
+        return true;
+    }
+
     //! 断言处理函数
-    inline void make_assert(bool pass, bool abort, std::jmp_buf env, const char* f, size_t n, const char* format, ...) {
+    static inline void make_assert(bool pass, bool abort, const char* file, size_t lineno, const char* format, ...) {
         // 如果执行未触发断言，不需要做什么
         if (pass) {
             return;
         }
 
         // 如果断言的条件无法满足，输出行号（这里的格式大部分 IDE 能自动解析）
-        std::printf("%s:%lu\n", f, n);
+        std::printf("%s:%lu\n", file, lineno);
 
         // 输出断言信息（这个实现得并不好，但是对于大多数项目来说够用了）
         va_list valist;
@@ -83,103 +108,94 @@ struct testcase : public testcase_head {
 
         // 如果指定了立即结束标记，那么通过 longjmp 回到 setjmp 的位置
         if (abort) {
-            std::longjmp(env, abort);
+            std::longjmp(curr_assertion_env, abort);
         }
     }
 
-    //! 执行一个测试用例
-    bool exec(bool& abort) {
-        // 每个用例执行前打印下当前正在执行那个任务
-        std::printf("%s[%s - %s] %s%s\n", tag_blue_enter, nsuite, ncase, "....", tag_blue_leave);
+    //! 执行所有的测试用例
+    static inline int exec_all(int argc, char* argv[]) {
+        // 记录用例数量
+        size_t count_total = 0;
+        size_t count_pass = 0;
+        size_t count_fail = 0;
 
-        // 设置 longjmp 的会跳点，以便于断言宏在必要时中断用例执行
-        std::jmp_buf assert_env;
-        if (setjmp(assert_env)) {
-            std::printf("%s[%s - %s] %s%s\n", tag_fail_enter, nsuite, ncase, "fail", tag_fail_leave);
-            abort = true;
-            return false;
-        }
+        // 按照注册顺序遍历用例
+        for (testcase_head* ptr = testcase::all_testcases.next; ptr != &testcase::all_testcases; ptr = ptr->next) {
+            count_total++;
+            bool abort = false;
 
-        // 执行用例
-        proc(this, assert_env);
+            // 如果用例执行失败，需要特殊处理
+            curr_testcase = reinterpret_cast<testcase*>(ptr);
+            if (!exec(curr_testcase, curr_assertion_env, abort)) {
 
-        // 如果执行成功，proc 函数会自动返回
-        std::printf("%s[%s - %s] %s%s\n", tag_pass_enter, nsuite, ncase, "pass", tag_pass_leave);
-        return true;
-    }
+                // 递增失败用例数量
+                count_fail++;
 
-    //! 所有测试用例表
-    static inline testcase_head all_testcases{};
-};
+                // 如果指定了退出标记，那么立即退出
+                if (abort) {
+                    break;
+                }
 
-//! 执行所有的测试用例
-static inline int exec_all(int argc, char* argv[]) {
-    // 记录用例数量
-    size_t count_total = 0;
-    size_t count_pass = 0;
-    size_t count_fail = 0;
-
-    // 按照注册顺序遍历用例
-    for (testcase_head* ptr = testcase::all_testcases.next; ptr != &testcase::all_testcases; ptr = ptr->next) {
-        count_total++;
-        bool abort = false;
-
-        // 如果用例执行失败，需要特殊处理
-        if (!(reinterpret_cast<testcase*>(ptr))->exec(abort)) {
-
-            // 递增失败用例数量
-            count_fail++;
-
-            // 如果指定了退出标记，那么立即退出
-            if (abort) {
-                break;
+                // 如果不需要退出，那么继续执行下一个用例
+                continue;
             }
 
-            // 如果不需要退出，那么继续执行下一个用例
-            continue;
+            // 如果执行成功就继续执行下一个用例
+            count_pass++;
         }
 
-        // 如果执行成功就继续执行下一个用例
-        count_pass++;
+        // 输出用例总结报告
+        std::printf("----------------------------\n");
+        if (count_fail == 0) {
+            std::printf("%sTotal : %lu%s\n", tag_pass_enter, count_total, tag_pass_leave);
+            std::printf("%sPass  : %lu%s\n", tag_pass_enter, count_pass, tag_pass_leave);
+            std::printf("%sFail  : %lu%s\n", tag_pass_enter, count_fail, tag_pass_leave);
+            return 0;
+        } else {
+            std::printf("%sTotal : %lu%s\n", tag_fail_enter, count_total, tag_fail_leave);
+            std::printf("%sPass  : %lu%s\n", tag_fail_enter, count_pass, tag_fail_leave);
+            std::printf("%sFail  : %lu%s\n", tag_fail_enter, count_fail, tag_fail_leave);
+            return 1;
+        }
     }
+};
 
-    // 输出用例总结报告
-    std::printf("----------------------------\n");
-    if (count_fail == 0) {
-        std::printf("%sTotal : %lu%s\n", tag_pass_enter, count_total, tag_pass_leave);
-        std::printf("%sPass  : %lu%s\n", tag_pass_enter, count_pass, tag_pass_leave);
-        std::printf("%sFail  : %lu%s\n", tag_pass_enter, count_fail, tag_pass_leave);
-        return 0;
-    } else {
-        std::printf("%sTotal : %lu%s\n", tag_fail_enter, count_total, tag_fail_leave);
-        std::printf("%sPass  : %lu%s\n", tag_fail_enter, count_pass, tag_fail_leave);
-        std::printf("%sFail  : %lu%s\n", tag_fail_enter, count_fail, tag_fail_leave);
-        return 1;
-    }
-}
-} // testing
+} // namespace testing
 
-// clang-format off
+//! 测试用例类
+#ifndef TESTING_TESTCACSE_CLASS
+#define TESTING_TESTCACSE_CLASS testing::testcase
+#endif // TESTING_TESTCACSE_CLASS
+
+//! 生成断言
+#ifndef TESTING_MAKE_ASSERT
+#define TESTING_MAKE_ASSERT TESTING_TESTCACSE_CLASS::make_assert
+#endif // TESTING_MAKE_ASSERT
+
 //! 定义一个测试用例的宏（很关键，有点啰嗦有点长）
-#define TEST(SuiteName_, CaseName_)                                                                                 \
-    static void testing_##SuiteName_##__##CaseName_(testing::testcase*, std::jmp_buf);                              \
-    static testing::testcase testing_register__##SuiteName_##__##CaseName_(&(testing_##SuiteName_##__##CaseName_),  \
-            (#SuiteName_), (#CaseName_), (__FILE__), (__LINE__));                                                   \
-    static void testing_##SuiteName_##__##CaseName_(testing::testcase* TESTCASE_, std::jmp_buf TEST_ASSERT_ENV_)    \
-    /* (end) */
-// clang-format on
+#ifndef TEST
+#define TEST(SuiteName_, CaseName_)                                                                                      \
+    static void testing_##SuiteName_##__##CaseName_();                                                                   \
+    static TESTING_TESTCACSE_CLASS testing_register__##SuiteName_##__##CaseName_(&(testing_##SuiteName_##__##CaseName_), \
+        (#SuiteName_), (#CaseName_), (__FILE__), (__LINE__));                                                            \
+    static void testing_##SuiteName_##__##CaseName_()
+#endif // TEST
 
 //! 测试用例的断言宏（基础断言宏）
-#define TESTING_ASSERT_(Expr_) ((TESTCASE_)->make_assert((Expr_), true, (TEST_ASSERT_ENV_), __FILE__, __LINE__, #Expr_))
-#define TESTING_EXPECT_(Expr_) ((TESTCASE_)->make_assert((Expr_), true, (TEST_ASSERT_ENV_), __FILE__, __LINE__, #Expr_))
+#ifndef TESTING_ASSERT
+#define TESTING_ASSERT(Expr_) (TESTING_MAKE_ASSERT((Expr_), true, __FILE__, __LINE__, #Expr_))
+#endif // TESTING_ASSERT
+#ifndef TESTING_EXPECT
+#define TESTING_EXPECT(Expr_) (TESTING_MAKE_ASSERT((Expr_), false, __FILE__, __LINE__, #Expr_))
+#endif // TESTING_EXPECT
 
 //! 扩展断言宏（模仿 gtest）
-#define EXPECT_EQ(Expr1_, Expr2_) (TESTING_EXPECT_(((Expr1_) == (Expr2_))))
-#define ASSERT_EQ(Expr1_, Expr2_) (TESTING_ASSERT_(((Expr1_) == (Expr2_))))
-#define EXPECT_TRUE(Expr1_) (TESTING_EXPECT_(!!(Expr1_)))
-#define ASSERT_TRUE(Expr1_) (TESTING_ASSERT_(!!(Expr1_)))
-#define EXPECT_FALSE(Expr1_) (TESTING_EXPECT_(!(Expr1_)))
-#define ASSERT_FALSE(Expr1_) (TESTING_ASSERT_(!(Expr1_)))
+#define EXPECT_EQ(Expr1_, Expr2_) (TESTING_EXPECT(((Expr1_) == (Expr2_))))
+#define ASSERT_EQ(Expr1_, Expr2_) (TESTING_ASSERT(((Expr1_) == (Expr2_))))
+#define EXPECT_TRUE(Expr1_) (TESTING_EXPECT(!!(Expr1_)))
+#define ASSERT_TRUE(Expr1_) (TESTING_ASSERT(!!(Expr1_)))
+#define EXPECT_FALSE(Expr1_) (TESTING_EXPECT(!(Expr1_)))
+#define ASSERT_FALSE(Expr1_) (TESTING_ASSERT(!(Expr1_)))
 
 //! 用例分段宏（模仿 catch2）
 #define SECTION(Desc_) if (true)
