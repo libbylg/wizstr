8190074d9268cc24cd82e95271cd615094ba9b71
libbylg@126.com <libbylg@126.com>
2025-05-24 23:31:41 +0800

    add testcase for encode_basexxx

--- test/test_str_encode_base16_inplace.cpp
+++ test/test_str_encode_base16_inplace.cpp
@@ -15,9 +15,9 @@
 
 TEST(test_str, encode_base16_inplace) {
     std::string s;
-    ASSERT_EQ(str::encode_base16_inplace(s, ""), "");
-    ASSERT_EQ(str::encode_base16_inplace(s, "a"), "61");
-    ASSERT_EQ(str::encode_base16_inplace(s, "ab"), "6162");
-    ASSERT_EQ(str::encode_base16_inplace(s, "abc"), "616263");
-    ASSERT_EQ(str::encode_base16_inplace(s, "abcXYZ", true), "61626358595A");
+    ASSERT_EQ(str::encode_base16_inplace(s = ""), "");
+    ASSERT_EQ(str::encode_base16_inplace(s = "a"), "61");
+    ASSERT_EQ(str::encode_base16_inplace(s = "ab"), "6162");
+    ASSERT_EQ(str::encode_base16_inplace(s = "abc"), "616263");
+    ASSERT_EQ(str::encode_base16_inplace(s = "abcXYZ", true), "61626358595A");
 }
