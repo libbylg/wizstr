ac53ee3f823a3ff70f5940d636048635579fce02
Liugang <libbylg@126.com>
2025-06-15 06:51:56 +0800

    add demos for align

---
+++ tests/test_str_all_demos.cpp
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2021-2025  libbylg@126.com
+ * str is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR
+ * FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+#include "testing.hpp"
+
+#include "str.hpp"
+
+#include <iostream>
+
+TEST(test_demo, align) {
+
+    std::string result;
+    for (int i = 11; i >= 1; i -= 2) {
+        result.append(str::align_center(str::repeat('*', i), 11)).append("\n");
+        // std::cout << str::align_center(str::repeat('*', i), 11) << std::endl;
+    }
+
+    std::string expect = //
+        "***********\n"  //
+        " ********* \n"  //
+        "  *******  \n"  //
+        "   *****   \n"  //
+        "    ***    \n"  //
+        "     *     \n";
+    ASSERT_EQ(result, expect);
+}
